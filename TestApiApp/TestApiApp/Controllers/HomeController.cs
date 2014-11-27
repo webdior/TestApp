096bedc987fc0860a5fbdc75efa0ab892b317822
webdior <sohit14all>
2014-11-27 12:16:29 +0530

    Test Api App

---
+++ TestApiApp/TestApiApp/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestApiApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [HttpPost]
+
+        public JsonResult mongod(string Xpos, string Ypos, string Uid, string Orientation, string Type, Models.RequestModels mrmodel)
+        {
+            try
+            {
+
+                string timestamp = Convert.ToString(DateTime.Now);
+                mrmodel.adddata(Uid, Xpos, Ypos, Orientation, Type, timestamp);
+
+                return Json(mrmodel);
+            }
+            catch (HttpException ex)
+            {
+                throw new HttpException(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        [HttpGet]
+        public ActionResult RequestData()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ TestApiApp/TestApiApp/Models/RequestModels.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+
+namespace TestApiApp.Models
+{
+    public class RequestModels
+    {
+        
+        [Required (ErrorMessage= "UserId is required.")]
+        public string uid { get; set; }
+       
+        public string xpos { get; set; }
+        public string ypos { get; set; }
+        public string orientation { get; set; }
+        public string type { get; set; }
+        public string timestamp { get; set; }
+        public string adddata(string Uid, string xpos, string ypos, string orientation, string type, string timestamp)//string id, string xpos, string ypos, string orientation, string type, string timestamp )
+        {
+            string apiurl = System.Configuration.ConfigurationManager.AppSettings["apiurl"]; 
+            ArrayList paramList = new ArrayList();
+            Product product = new Product { UserId  = Uid  ,  xpos = xpos, ypos= ypos, orientation = orientation, type = type, timestamp = timestamp }; 
+      
+            paramList.Add(product);
+               
+            HttpClient httpClient = new HttpClient();
+      
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = httpClient.PostAsJsonAsync(apiurl + "api/Apis/add/", paramList).Result;
+
+            string valuetest = response.Content.ReadAsAsync<string>().Result;
+            return valuetest;
+        }
+    
+    }
+  
+}
